3602056c3cea676472ded7bfddc44c19a479c736
Hao Hanqi <haohanqi@Haos-MacBook-Pro.local>
2020-10-23 14:06:00 -0400

    change header

--- src/components/header.js
+++ src/components/header.js
@@ -1,7 +1,6 @@
 import { Link } from "gatsby"
 import React from "react"
 import {Row,Col} from 'antd'
-import {NavWapper,NavItem,Logo} from './basicStyle'
 
 const Header = ({  }) => (
   <NavWapper>
